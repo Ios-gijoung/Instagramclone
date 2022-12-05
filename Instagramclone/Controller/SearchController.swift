401e3b57a4cbe3e5cd902e66f76284779254ab69
Ios-gijoung <hell-er14@naver.com>
2022-12-05 19:49:38 +0900

    test

--- Instagramclone/Controller/SearchController.swift
+++ Instagramclone/Controller/SearchController.swift
@@ -11,7 +11,10 @@ class SearchController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         
+        
+    
         view.backgroundColor = .systemPink
     }
 }
